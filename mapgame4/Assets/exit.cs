1b2b97c7f7d3ed0819c336828f5170fab53418a7
unknown <313511528@qq.com>
2018-08-31 12:59:09 +0800

    修改

---
+++ mapgame4/Assets/exit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class exit : MonoBehaviour {
+
+    public GameObject OkBtn;
+    // Use this for initialization
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ mapgame4/Assets/npc2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc2 : MonoBehaviour {
+    public GameObject Talk1;
+    public GameObject Talk2;
+    public GameObject Talk3;
+    public GameObject Panel;
+    // Use this for initialization
+    void Start () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Talk1.SetActive(true);
+            Panel.SetActive(true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Talk1.SetActive(false);
+            Talk2.SetActive(true);
+            Panel.SetActive(true);
+        
+
+        }
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.T))
+        {
+            Talk3.SetActive(true);
+            Talk2.SetActive(false);
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Talk1.SetActive(false);
+            Talk2.SetActive(false);
+            Talk3.SetActive(false);
+            Panel.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
